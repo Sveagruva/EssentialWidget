5a0430ad647e88d294a009ac9fa8a713419285b8
Sveagruva <55844843+Sveagruva@users.noreply.github.com>
2022-09-13 21:57:21 +0400

    Initial Commit

---
+++ EssentialWidget/EssentialWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialWidgetApp.swift
+//  EssentialWidget
+//
+//  Created by Sveagruva on 13.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct EssentialWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
